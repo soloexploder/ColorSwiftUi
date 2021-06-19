bf9a7ebb7300006bbc1235f82f63511c24fe251b
Khanturov Daniil <soloexploder@gmail.com>
2021-06-19 12:38:28 +0300

    Initial Commit

---
+++ ColorSwiftUi/ColorSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwiftUiApp.swift
+//  ColorSwiftUi
+//
+//  Created by Даниил Хантуров on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
